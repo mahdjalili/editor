98de7335b7d90145f7e26875ec5e9b4d48b0cd53
Mahdi <mahdiwise@gmail.com>
2024-12-26 13:21:31 +0330

    add undo and redo, layers changes history

--- app/studio/_components/header/Header.js
+++ app/studio/_components/header/Header.js
@@ -1,7 +1,7 @@
 import styles from "./header.module.css";
 
 import { DownloadOutlined } from "@ant-design/icons";
-import { Breadcrumb, Button, Switch } from "antd";
+import { Breadcrumb, Button, Switch, Tooltip } from "antd";
 import { useEditor } from "@/providers/EditorProvider";
 import { useAnt } from "@/providers/AntProvider";
 
@@ -12,6 +12,10 @@ export default function Header() {
     const editor = useEditor();
     const stageRef = editor.stageRef;
     const [, setSelectedLayerId] = editor.selectedLayerId;
+
+    const history = editor.history;
+    const undo = history.undo;
+    const redo = history.redo;
 
     const downloadURI = (uri, name) => {
         var link = document.createElement("a");
@@ -50,12 +54,21 @@ export default function Header() {
             </div>
 
             <div>
+                <Tooltip title="بازگردانی آخرین تغییرات">
+                    <Button onClick={undo} type="text" icon={<i className="fa-regular fa-undo"></i>}></Button>
+                </Tooltip>
+
+                <Tooltip title="تکرار آخرین تغییرات">
+                    <Button onClick={redo} type="text" icon={<i className="fa-regular fa-redo"></i>}></Button>
+                </Tooltip>
+
                 <Switch
                     checkedChildren="نم روشن"
                     unCheckedChildren="تم تاریک"
                     onChange={(value) => setTheme(value ? "dark" : "light")}
                     defaultChecked
                 />
+
                 <Button onClick={handleExport} type="primary" icon={<DownloadOutlined />} size="large" />
             </div>
         </div>
--- providers/EditorProvider.js
+++ providers/EditorProvider.js
@@ -7,7 +7,7 @@ import axios from "@/utils/axios";
 import WebFont from "webfontloader";
 import { layers as listOfLayers } from "@/layers/layers";
 import { templatesConverter, defaultTemplate } from "@/templates/templates";
-import { isEmpty } from "lodash";
+import { isEmpty, set } from "lodash";
 
 export const EditorContext = createContext();
 
@@ -16,19 +16,34 @@ export const EditorProvider = (props) => {
 
     const editorContext = {};
     editorContext.stageRef = useRef(null);
+
     editorContext.listOfLayers = listOfLayers;
+
     editorContext.selectedTemplate = useState(defaultTemplate);
     editorContext.layers = useState(defaultTemplate.layers);
-    editorContext.selectedLayerId = useState(null);
+    editorContext.selectedLayerId = useState(defaultTemplate.layers[0].id);
 
     const [selectedTemplate, setSelectedTemplate] = editorContext.selectedTemplate;
     const [layers, setLayers] = editorContext.layers;
     const [selectedLayerId, setSelectedLayerId] = editorContext.selectedLayerId;
 
+    editorContext.history = {};
+    editorContext.history.layers = useState([defaultTemplate.layers]);
+    editorContext.history.step = useState(0);
+
+    const [history, setHistory] = editorContext.history.layers;
+    const [historyStep, setHistoryStep] = editorContext.history.step;
+
     editorContext.changeTemplate = (template) => {
-        console.log("Change Template: ", template);
+        console.log("Template Has Been Changed: ", template);
+
+        skipEffectRef.current = true;
         setSelectedTemplate(template);
         setLayers(template.layers);
+        setSelectedLayerId(template.layers[0].id);
+
+        setHistory([template.layers]);
+        setHistoryStep(0);
     };
 
     const template = useQuery({
@@ -47,9 +62,9 @@ export const EditorProvider = (props) => {
         }
     }, [template.isSuccess]);
 
+    // Load google fonts dynamically when layers change
     useEffect(() => {
         console.log("Layers Changed:", layers);
-
         let fontFamilies = layers.filter((layer) => layer.type == "text").map((layer) => layer.fontFamily);
         if (!isEmpty(fontFamilies)) {
             WebFont.load({
@@ -60,14 +75,62 @@ export const EditorProvider = (props) => {
         }
     }, [layers]);
 
-    const handleDeleteLayer = (event) => {
-        if (event.key === "Delete" && selectedLayerId) {
-            setLayers((prevLayers) => prevLayers.filter((layer) => layer.id !== selectedLayerId));
-            setSelectedLayerId(null);
+    // Updating the history when layers change
+    const skipEffectRef = useRef(false);
+    useEffect(() => {
+        if (skipEffectRef.current) {
+            // Skip the effect when undo/redo is called
+            skipEffectRef.current = false;
+            return;
         }
+
+        setHistoryStep((step) => {
+            let nextStep = step + 1;
+            setHistory((prevHistory) => {
+                let newHistory = prevHistory;
+                newHistory[nextStep] = layers;
+                return newHistory;
+            });
+            return nextStep;
+        });
+    }, [layers]);
+
+    useEffect(() => {
+        console.log("History Step:", historyStep, "History:", history);
+    }, [historyStep]);
+
+    editorContext.history.undo = () => {
+        if (historyStep === 0) {
+            return;
+        }
+
+        skipEffectRef.current = true;
+        setHistoryStep((step) => {
+            setLayers(history[step - 1]);
+            return step - 1;
+        });
+    };
+
+    editorContext.history.redo = () => {
+        if (historyStep === history.length - 1) {
+            return;
+        }
+
+        skipEffectRef.current = true;
+        setHistoryStep((step) => {
+            setLayers(history[step + 1]);
+            return step + 1;
+        });
     };
 
     useEffect(() => {
+        const handleDeleteLayer = (event) => {
+            if (event.key === "Delete" && selectedLayerId) {
+                setLayers((prevLayers) => prevLayers.filter((layer) => layer.id !== selectedLayerId));
+                setSelectedLayerId(null);
+            }
+        };
+
         window.addEventListener("keydown", handleDeleteLayer);
         return () => {
             window.removeEventListener("keydown", handleDeleteLayer);
